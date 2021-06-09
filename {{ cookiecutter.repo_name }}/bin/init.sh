fd1c984cad8d2125f9cd332c35949b273f27d309
agaszmurlo <amszmurlo@gmail.com>
2021-06-09 14:11:01 +0200

    update init.sh

--- {{ cookiecutter.repo_name }}/bin/init.sh
+++ {{ cookiecutter.repo_name }}/bin/init.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "Creating venv for ML project"
 source /opt/conda/etc/profile.d/conda.sh
 PROJECT_NAME=ds-project
 VENV_DIR=$HOME/work/venv/$PROJECT_NAME
-conda create python=$PYTHON_MINOR -p $VENV_DIR -y 
-conda activate $VENV_DIR
-pip install kedro==$KEDRO_VERSION
+if [ ! -d $VENV_DIR ] 
+then
+        echo "Creating venv for ML project"
+        conda create python=$PYTHON_MINOR -p $VENV_DIR -y 
+        conda activate $VENV_DIR
+        pip install kedro==$KEDRO_VERSION
+else
+        echo "Venv for ML project already exists: $VENV_DIR "
+        conda activate $VENV_DIR
+fi
+
 
 CONFIG_FILE=config.yaml
 cat <<EOF >> $CONFIG_FILE
@@ -25,25 +32,31 @@ sed -i 's/mlflow_tracking_uri: mlruns/mlflow_tracking_uri: http:\/\/localhost:50
 cp conf/local/mlflow.yml conf/local-spark/
 conda deactivate
 
-
-echo "Creating Jupyter kernel"
 KERNEL_NAME=ds-venv
-ipython kernel install --name $KERNEL_NAME --display-name "Kedro (datascience)" --user
-echo -e "
-{
-\"argv\": [
-\"$VENV_DIR/bin/python3\",
-\"-m\",
-\"ipykernel_launcher\",
-\"-f\",
-\"{connection_file}\",
-\"--ipython-dir\",
-\"$HOME/work/git/$PROJECT_NAME/.ipython\"
-],
-\"env\":{
-    \"KEDRO_ENV\": \"local-spark\",
-    \"PYTHONPATH\": \"$PYTHONPATH:$HOME/work/git/$PROJECT_NAME/src\" 
-},
-\"display_name\": \"Kedro (datascience)\",
-\"language\": \"python\"
-}" > $HOME/.local/share/jupyter/kernels/$KERNEL_NAME/kernel.json
+KERNEL_FILE=$HOME/.local/share/jupyter/kernels/$KERNEL_NAME/kernel.json
+if [ ! -f $KERNEL_FILE ] 
+then
+        echo "Creating Jupyter kernel"
+        ipython kernel install --name $KERNEL_NAME --display-name "Kedro (datascience)" --user
+        echo -e "
+        {
+        \"argv\": [
+        \"$VENV_DIR/bin/python3\",
+        \"-m\",
+        \"ipykernel_launcher\",
+        \"-f\",
+        \"{connection_file}\",
+        \"--ipython-dir\",
+        \"$HOME/work/git/$PROJECT_NAME/.ipython\"
+        ],
+        \"env\":{
+        \"KEDRO_ENV\": \"local-spark\",
+        \"PYTHONPATH\": \"$PYTHONPATH:$HOME/work/git/$PROJECT_NAME/src\" 
+        },
+        \"display_name\": \"Kedro (datascience)\",
+        \"language\": \"python\"
+        }" > $KERNEL_FILE
+else
+        echo "Jupyter kernel already exists: $KERNEL_FILE"
+fi
+conda activate $VENV_DIR
